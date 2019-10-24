19b0bdf766265134191fbeedef6616463fe9154c
Wilfredo Machado <wilfredo.machado@correo.unimet.edu.ve>
2019-10-24 01:28:11 -0400

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { WhatWeOfferComponent } from './components/HomePage/what-we-offer/what-w
 import { WhatWeOfferSecondComponent } from './components/HomePage/what-we-offer-second/what-we-offer-second.component';
 import { MenuDesktopComponent } from './components/HomePage/menu-desktop/menu-desktop.component';
 import { TouristInfoComponent } from './components/TouristInfo/tourist-info/tourist-info.component';
+import { BarComponent } from './components/HotelsList/bar/bar.component';
+import { FiltroComponent } from './components/HotelsList/filtro/filtro.component';
+import { HeaderComponent } from './components/HotelsList/header/header.component';
+import { HotelComponent } from './components/HotelsList/hotel/hotel.component';
+import { MenuComponent } from './components/HotelsList/menu/menu.component';
+import { TooltipModule } from 'ng2-tooltip-directive';
 
 @NgModule({
   declarations: [
@@ -17,12 +23,18 @@ import { TouristInfoComponent } from './components/TouristInfo/tourist-info/tour
     WhatWeOfferComponent,
     WhatWeOfferSecondComponent,
     MenuDesktopComponent,
-    TouristInfoComponent
+    TouristInfoComponent,
+    BarComponent,
+    FiltroComponent,
+    HeaderComponent,
+    HotelComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    TooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
