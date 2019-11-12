6fdbada56b7b187e0664f1b9ccecfd9377f12802
Wilfredo Machado <wilfredo.machado@correo.unimet.edu.ve>
2019-11-11 21:41:33 -0400

    qwerty

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDT8jQLvlVbhYlj4pLNjfKxXnYuWYIeaVA",
-    authDomain: "venezuela-tours-8a4d9.firebaseapp.com",
-    databaseURL: "https://venezuela-tours-8a4d9.firebaseio.com",
-    projectId: "venezuela-tours-8a4d9",
-    storageBucket: "venezuela-tours-8a4d9.appspot.com",
-    messagingSenderId: "823719035614",
-    appId: "1:823719035614:web:a11e8cccb5895aac43784f",
-    measurementId: "G-ZB0WRTBHVW"
+    apiKey: "AIzaSyBvg4gyS_V6uMdrlAETa-prY7gAuAAAjI8",
+    authDomain: "venezuela-travel-4a94e.firebaseapp.com",
+    databaseURL: "https://venezuela-travel-4a94e.firebaseio.com",
+    projectId: "venezuela-travel-4a94e",
+    storageBucket: "venezuela-travel-4a94e.appspot.com",
+    messagingSenderId: "109262838071",
+    appId: "1:109262838071:web:87abcb2db8a771135062b2",
+    measurementId: "G-WGW7NWW0Q8"
   }
 
 };
