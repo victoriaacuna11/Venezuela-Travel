cc6c3f6f6baa0857c9785314605ffaab4b95d569
Romel Clavel <romeletoty@gmail.com>
2019-11-13 23:02:56 -0400

    login con el margen raro pero listo

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public lgUser="romel";
+  public lgPassword="123";
+
+  public username:any;
+  public password:any;
+
+  public property=false;
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  
+  onLogin(){
+
+    if(this.lgUser==this.username && this.lgPassword==this.password){
+
+      console.log(this.username+this.password);
+
+      this.router.navigate(['/']);
+
+    }else{
+      
+      this.property=true;
+      
+    }
+
+    
+
+}
+
+}
